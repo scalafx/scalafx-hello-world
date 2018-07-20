d8fce1a7825d782814b15e98c55fe5dc8d0876f0
Jengel1 <38148097+Jengel1@users.noreply.github.com>
2018-07-20 15:13:54 -0600

    Changed ScalaFXHelloWorld file to different scene

--- src/main/scala/hello/ScalaFXHelloWorld.scala
+++ src/main/scala/hello/ScalaFXHelloWorld.scala
@@ -31,45 +31,16 @@ import scalafx.application.JFXApp
 import scalafx.application.JFXApp.PrimaryStage
 import scalafx.geometry.Insets
 import scalafx.scene.Scene
-import scalafx.scene.effect.DropShadow
-import scalafx.scene.layout.HBox
-import scalafx.scene.paint.Color._
-import scalafx.scene.paint._
-import scalafx.scene.text.Text
-
-object ScalaFXHelloWorld extends JFXApp {
-
+import scalafx.scene.control.Label
+import scalafx.scene.layout.BorderPane
+ 
+object HelloSBT extends JFXApp {
   stage = new PrimaryStage {
-    //    initStyle(StageStyle.Unified)
-    title = "ScalaFX Hello World"
     scene = new Scene {
-      fill = Color.rgb(38, 38, 38)
-      content = new HBox {
-        padding = Insets(50, 80, 50, 80)
-        children = Seq(
-          new Text {
-            text = "Scala"
-            style = "-fx-font: normal bold 100pt sans-serif"
-            fill = new LinearGradient(
-              endX = 0,
-              stops = Stops(Red, DarkRed))
-          },
-          new Text {
-            text = "FX"
-            style = "-fx-font: italic bold 100pt sans-serif"
-            fill = new LinearGradient(
-              endX = 0,
-              stops = Stops(White, DarkGray)
-            )
-            effect = new DropShadow {
-              color = DarkGray
-              radius = 15
-              spread = 0.25
-            }
-          }
-        )
+      root = new BorderPane {
+        padding = Insets(25)
+        center = new Label("Hello SBT")
       }
     }
-
   }
 }
