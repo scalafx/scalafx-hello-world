2baa0f156a501130da26db96d40b346e9a7a40b6
Jarek Sacha <jpsacha@gmail.com>
2021-04-22 19:27:28 -0400

    Remove "{}" not needed in Scala 3 for more compact notation

--- src/main/scala/hello/ScalaFXHelloWorld.scala
+++ src/main/scala/hello/ScalaFXHelloWorld.scala
@@ -12,42 +12,29 @@ import scalafx.scene.text.Text
 
 import scala.language.implicitConversions
 
-object ScalaFXHelloWorld extends JFXApp3 {
+object ScalaFXHelloWorld extends JFXApp3 :
 
-  override def start(): Unit = {
+  override def start(): Unit =
 
-    stage = new PrimaryStage {
+    stage = new PrimaryStage :
       //    initStyle(StageStyle.Unified)
       title = "ScalaFX Hello World"
-      scene = new Scene {
+      scene = new Scene :
         fill = Color.rgb(38, 38, 38)
-        content = new HBox {
+        content = new HBox :
           padding = Insets(50, 80, 50, 80)
           children = Seq(
-            new Text {
+            new Text :
               text = "Scala"
               style = "-fx-font: normal bold 100pt sans-serif"
-              fill = new LinearGradient(
-                endX = 0,
-                stops = Stops(Red, DarkRed))
-            },
-            new Text {
+              fill = new LinearGradient(endX = 0, stops = Stops(Red, DarkRed))
+            ,
+            new Text :
               text = "FX"
               style = "-fx-font: italic bold 100pt sans-serif"
-              fill = new LinearGradient(
-                endX = 0,
-                stops = Stops(White, DarkGray)
-              )
-              effect = new DropShadow {
+              fill = new LinearGradient(endX = 0, stops = Stops(White, DarkGray))
+              effect = new DropShadow :
                 color = DarkGray
                 radius = 15
                 spread = 0.25
-              }
-            }
-          )
-        }
-      }
-    }
-
-  }
-}
+            )
