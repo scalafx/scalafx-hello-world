ffc1339392054d79605af9c5c84f268aa1fd160c
Jarek Sacha <jpsacha@gmail.com>
2021-05-17 20:45:30 -0400

    JFXApp is deprecated, use JFXApp3

--- src/main/scala/hello/ScalaFXHelloWorld.scala
+++ src/main/scala/hello/ScalaFXHelloWorld.scala
@@ -1,7 +1,6 @@
 package hello
 
-import scalafx.application.JFXApp
-import scalafx.application.JFXApp.PrimaryStage
+import scalafx.application.JFXApp3
 import scalafx.geometry.Insets
 import scalafx.scene.Scene
 import scalafx.scene.effect.DropShadow
@@ -10,39 +9,39 @@ import scalafx.scene.paint.Color._
 import scalafx.scene.paint._
 import scalafx.scene.text.Text
 
-object ScalaFXHelloWorld extends JFXApp {
-
-  stage = new PrimaryStage {
-    //    initStyle(StageStyle.Unified)
-    title = "ScalaFX Hello World"
-    scene = new Scene {
-      fill = Color.rgb(38, 38, 38)
-      content = new HBox {
-        padding = Insets(50, 80, 50, 80)
-        children = Seq(
-          new Text {
-            text = "Scala"
-            style = "-fx-font: normal bold 100pt sans-serif"
-            fill = new LinearGradient(
-              endX = 0,
-              stops = Stops(Red, DarkRed))
-          },
-          new Text {
-            text = "FX"
-            style = "-fx-font: italic bold 100pt sans-serif"
-            fill = new LinearGradient(
-              endX = 0,
-              stops = Stops(White, DarkGray)
-            )
-            effect = new DropShadow {
-              color = DarkGray
-              radius = 15
-              spread = 0.25
+object ScalaFXHelloWorld extends JFXApp3 {
+  override def start(): Unit = {
+    stage = new JFXApp3.PrimaryStage {
+      //    initStyle(StageStyle.Unified)
+      title = "ScalaFX Hello World"
+      scene = new Scene {
+        fill = Color.rgb(38, 38, 38)
+        content = new HBox {
+          padding = Insets(50, 80, 50, 80)
+          children = Seq(
+            new Text {
+              text = "Scala"
+              style = "-fx-font: normal bold 100pt sans-serif"
+              fill = new LinearGradient(
+                endX = 0,
+                stops = Stops(Red, DarkRed))
+            },
+            new Text {
+              text = "FX"
+              style = "-fx-font: italic bold 100pt sans-serif"
+              fill = new LinearGradient(
+                endX = 0,
+                stops = Stops(White, DarkGray)
+              )
+              effect = new DropShadow {
+                color = DarkGray
+                radius = 15
+                spread = 0.25
+              }
             }
-          }
-        )
+          )
+        }
       }
     }
-
   }
 }
